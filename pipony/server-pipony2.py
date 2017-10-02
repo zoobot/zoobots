7642d8d9e5bae0027a4350fed2045cb985f71d70
botzoo <rommims@gmail.com>
2017-10-02 13:01:54 -0700

    test

--- pipony/server-pipony2.py
+++ pipony/server-pipony2.py
@@ -6,16 +6,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 from flask import Flask, request, send_from_directory
 
-# Sets up pins as outputs
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    # Define GPIO pins
-    Motor1A = 27
-    Motor1B = 24
-    Motor1Enable = 5
-    GPIO.setup(Motor1A,GPIO.OUT)
-    GPIO.setup(Motor1B,GPIO.OUT)
-    GPIO.setup(Motor1Enable,GPIO.OUT)
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -56,15 +46,15 @@ def disconnect(sid):
     GPIO.cleanup()
     print('disconnect ', sid)
 
-
-
-try:
-    main()
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
-
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    # Define GPIO pins
+    Motor1A = 27
+    Motor1B = 24
+    Motor1Enable = 5
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1Enable,GPIO.OUT)
 
 if __name__ == '__main__':
     setup()
@@ -74,8 +64,9 @@ if __name__ == '__main__':
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
 
-    try:
-      print('in try')
-    # Stop on Ctrl+C and clean up
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+try:
+    main()
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
