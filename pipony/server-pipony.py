fdef86154f34e66efa3f4205f55a33be665540ef
botzoo <rommims@gmail.com>
2017-10-02 20:29:03 -0700

    using gpiozero

--- pipony/server-pipony.py
+++ pipony/server-pipony.py
@@ -1,83 +1,92 @@
 import socketio
 import eventlet
-import RPi.GPIO as GPIO
-from time import sleep
-# from flask import Flask, render_template
-from flask import Flask, request, send_from_directory
+import eventlet.wsgi
 
-io = socketio.Server()
+import RPi.GPIO as GPIO
+from gpiozero import Motor, OutputDevice
+from time import sleep
+
+from flask import Flask, request, send_from_directory
+sio = socketio.Server()
 app = Flask(__name__)
 
-# Set the GPIO mode
-GPIO.setmode(GPIO.BCM)
+motor1 = Motor(24, 27)
+motor1_enable = OutputDevice(5, initial_value=1)
+# motor2 = Motor(6, 22)
+# motor2_enable = OutputDevice(17, initial_value=1)
+# motor3 = Motor(23, 16)
+# motor3_enable = OutputDevice(12, initial_value=1)
+motor4 = Motor(13, 18)
+motor4_enable = OutputDevice(25, initial_value=1)
 
-# Define GPIO pins
-Motor1A = 27
-Motor1B = 24
-Motor1Enable = 5
+@app.route('/<path:path>', methods=['POST', 'GET'])
+def serve_page(path):
+  return send_from_directory('static', path)
 
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1Enable,GPIO.OUT)
+@sio.on('connect')
+def connect(sid, data):
+    print("connected ", data, sid)
+    sio.emit('room', room=sid)
 
-# @app.route('/')
-# def index():
-#     """Serve the client-side application."""
-#     return render_template('client/index.html')
-
-#use this for local static file server
-#don't need it with remote nginx frontend
-# @app.route('/<path:path>', methods=['POST', 'GET'])
-# def serve_page(path):
-#   # return index for get
-#   return send_from_directory('static', path)
-
-@io.on('connect')
-def connect(sid, environ):
-    print('connect ', sid)
-
-# @io.on('my message')
-# def message(sid, data):
-#     print('message ', data)
-
-@io.on('robot-forward')
+@sio.on('robot-forward')
 def message(sid):
     # Turn the motor on
-    GPIO.output(Motor1A,GPIO.HIGH) # GPIO high to send power to the + terminal
-    GPIO.output(Motor1B,GPIO.LOW) # GPIO low to ground the - terminal
-    GPIO.output(Motor1Enable,GPIO.HIGH) # GPIO high to enable this motor
+    motor1.value = 0.5 # half speed forwards
+    motor4.value = 0.5 # half speed forwards
     print('message ', sid)
 
-@io.on('robot-back')
+@sio.on('robot-back')
 def message(sid):
-    GPIO.output(Motor1A,GPIO.LOW) # GPIO high to send power to the + terminal
-    GPIO.output(Motor1B,GPIO.HIGH) # GPIO low to ground the - terminal
-    GPIO.output(Motor1Enable,GPIO.HIGH) # GPIO high to enable this motor
+    motor1.value = -0.5 # half speed backwards
+    motor4.value = -0.5 # half speed backwards
     print('message ', sid)
 
-@io.on('robot-stop')
+@sio.on('robot-stop')
 def message(sid):
     # Stop the motor by 'turning off' the enable GPIO pin
-    GPIO.output(Motor1Enable,GPIO.LOW)
+    motor1.value = 0 # stop
+    motor4.value = 0 # stop
+    print('message ', sid)
+
+@sio.on('robot-right')
+def message(sid):
+    # Stop the motor by 'turning off' the enable GPIO pin
+    motor1.value = 1 # right
+    motor4.value = 0.25 # right
+    print('message ', sid)
+
+@sio.on('robot-left')
+def message(sid):
+    # Stop the motor by 'turning off' the enable GPIO pin
+    motor1.value = .25 # left
+    motor4.value = 1 # left
+    print('message ', sid)
+
+@sio.on('robot-stop')
+def message(sid):
+    # Stop the motor by 'turning off' the enable GPIO pin
+    motor1.value = 0 # stop
+    motor4.value = 0 # stop
     print('message ', sid)
 
 
-@io.on('disconnect')
+@sio.on('disconnect')
 def disconnect(sid):
-    # Always end this script by cleaning the GPIO
     GPIO.cleanup()
     print('disconnect ', sid)
 
+def main():
+    print('main')
+
 if __name__ == '__main__':
-    # wrap Flask application with socketio's middleware
-    app = socketio.Middleware(io, app)
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
-
-try:
-    main()
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
